d1db56c4b973c4ddcb52d436229870c0096d1cdf
Jan Eris Saludo <janerissaludo@gmail.com>
2023-02-11 23:47:47 +0800

    added destination ejs - Finished

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
+const fs = require('fs');
 const app = express();
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}))
@@ -13,12 +14,32 @@ app.get("/", (req,res)=>{
 })
 
 app.get("/home", (req,res)=>{
-    res.render('index');
+    res.render('index', {bgImg: "/images/home/background-home-desktop.jpg"});
 })
-app.get("/destination-moon", (req,res) =>{
-    res.render('destination-moon');
+
+
+app.get("/destination-:topic", (req,res) =>{
+    fs.readFile(__dirname + "/data.json", (err, data) => {
+        if (err) throw err;
+        var tempData = JSON.parse(data);
+        
+        var destinationList = ["moon", "mars", "europa", "titan"];
+        let i = destinationList.indexOf(req.params.topic);
+       
+        res.render('destination', {bgImg: "/images/destination/background-destination-desktop.jpg", 
+        destImg: tempData.destinations[i].images.png,
+        name: tempData.destinations[i].name,
+        description: tempData.destinations[i].description,
+        distance: tempData.destinations[i].distance,
+        travel: tempData.destinations[i].travel
+        });
+        
+    });
+   
 })
+
 
 app.listen(3000, function(){
     console.log("Server started in port 3000");
 })
+
--- public/js/animation.js
+++ public/js/animation.js
@@ -2,9 +2,29 @@ let navList = document.querySelector("#nav-list");
 
 let activePage = window.location.pathname;
 
-console.log(activePage);
+
 let navLink = navList.querySelectorAll("a").forEach(link  => {
         if(link.href.includes(`${activePage}`)){
             link.classList.add("active");
         }
-    }); 
+
+ 
+}); 
+
+let destinationList = document.querySelector("#destination-nav");
+
+
+
+
+let destinationNavLink = destinationList.querySelectorAll("a").forEach(link  => {
+   
+    if(link.href.includes(`${activePage}`)){
+                
+            link.classList.add("active");
+        }
+
+ 
+}); 
+
+
+    
